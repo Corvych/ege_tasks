3b6c849cbee7b0c5e3da45b7dd50a6d24648bf98
Karatonov Kirill <salvador7272@yandex.ru>
2023-02-27 19:10:33 +0300

    Задача 24.4

---
+++ Задача 24/4/main.py
@@ -0,0 +1,13 @@
+f = open('4.txt')
+r = f.read()
+
+count = 0
+cmax = 0
+
+for i in range(0, len(r)-1):
+    if r[i] != r[i-1] and r[i] != r[i+1]:
+        count += 1
+        cmax = max(count, cmax)
+    else:
+        count = 1
+print(cmax)
