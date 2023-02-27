46074942ba13d53ee886b3522fd1f84225b51ca3
Karatonov Kirill <salvador7272@yandex.ru>
2023-02-27 18:20:38 +0300

    Исправил задание 24.2, main.py

--- Задача 24/2/main.py
+++ Задача 24/2/main.py
@@ -5,8 +5,10 @@ count = 0
 cmax = 0
 
 for i in range(0, len(r)):
-    if r[i] != "Z":
-        count+=1
+    if r[i] == "Z":
         cmax = max(count, cmax)
+        count = 0
+    else:
+        count += 1
 
 print(cmax)
